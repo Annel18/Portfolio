84d92d1b8fe7f59e49e3884c6fd4074e57cb90fe
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-01-30 13:03:56 +0000

    interest section updated

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,7 @@
 // import { Link } from 'react-router-dom'
 import { Container, Row, Col } from "react-bootstrap"
 import knitLogo from '../assets/icons/knittingLogo.svg'
-import musicLogo from '../assets/icons/musicLogo.svg'
+import circuit from '../assets/icons/circuit.svg'
 
 
 
@@ -15,9 +15,9 @@ export default function Interests() {
             description: '<p>Since learning knitting two years ago, I have been making many clothes (hats, scarves, cardigans, hoodies..) and have become obsessed with learning new techniques and creating new patterns.</p>'
         },
         {
-            title: 'Music',
-            img: musicLogo,
-            description: '<p>I play the piano and violin</p>'
+            title: 'Electronics',
+            img: circuit,
+            description: '<p>I enjoy fiddling with Processing and Arduino and I recently made a touch lamp with conductive paint, LED strips and frosted glass. Playing with Arduino breadboards kept me busy in lockdown.</p>'
         },
     ]
 
@@ -36,10 +36,8 @@ export default function Interests() {
                                             sm={6}
                                         // style={{alignItems:'flex-end'}}
                                         >
-                                            <div className="logo-container">
-                                                {/* <div className="poster" style={{ backgroundImage: `url(${project.img})` }}></div> */}
-                                                {/* <div className="poster" style={{ backgroundImage: `url(${exp.img})`, textAlign:'rigth' }}></div> */}
-                                                <img className="logo" src={interest.img} alt={interest.img} />
+                                            <div className="logo-container" style={{height:'5rem'}}>
+                                                <img className="logo" src={interest.img} alt={interest.img} style={{height:'10rem'}}/>
                                             </div>
                                         </Col>
                                         <Col >
@@ -62,8 +60,8 @@ export default function Interests() {
                                             className="single-container"
                                             sm={6}
                                         >
-                                            <div style={{textAlign:'left'}} className="logo-container">
-                                                <img className="logo" src={interest.img} alt={interest.img} />
+                                            <div style={{textAlign:'left', height:'5rem'}} className="logo-container">
+                                                <img className="logo" src={interest.img} alt={interest.img} style={{height:'8rem'}}/>
                                             </div>
                                         </Col>
                                     </Row>
