07155765d07e660cbc8d5592c2b394fd10ded478
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-01-30 16:17:02 +0000

    nav bar offcanvas when screen is small

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,55 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 export default function NavBar({ navLinks, bckgImg }) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    function Example() {
+        return (
+            <>
+                {['start', 'end', 'top', 'bottom'].map((placement, idx) => (
+                    <OffCanvasExample key={idx} placement={placement} name={placement} />
+                ))}
+            </>
+        );
+    }
 
     return (
         <>
-            <nav style={{zIndex:'2'}}>
+            <nav className='navBar-large'>
                 <ul>
                     <li>
                         {navLinks.map((link, i) => <a key={i} href={`#${link.toLowerCase()}`}>{link} </a>)}
                     </li>
                 </ul>
             </nav>
-            {/* <nav> */}
-            {/* <img src={bckgImg} alt={bckgImg} style={{ width: '100vw', height:'100vh', zIndex:'-2' , position: 'sticky' }}/> */}
-            {/* <div style={{ backgroundImage: `url(${bckgImg})`, backgroundSize: 'contain', height: '100vh', width: '100vw', position: 'sticky', zIndex:'-2'}}></div> */}
-            {/* </nav> */}
+            <div className='navBar-small navBar'>
+                <div variant="primary" onClick={handleShow} className="me-2">
+                    <div className='navButton'></div>
+                    <div className='navButton'></div>
+                    <div className='navButton'></div>
+                </div>
+                <Offcanvas show={show} onHide={handleClose} placement={'top'}>
+                    {/* <Offcanvas.Header closeButton>
+                        <Offcanvas.Title></Offcanvas.Title>
+                    </Offcanvas.Header> */}
+                    <Offcanvas.Body>
+                        <nav>
+                            <ul>
+                                <li>
+                                    {navLinks.map((link, i) => <a key={i} href={`#${link.toLowerCase()}`}>{link} </a>)}
+                                </li>
+                            </ul>
+                        </nav>
+                    </Offcanvas.Body>
+                </Offcanvas>
+            </div>
+
         </>
     )
 }
