010039b4796d4bfd47e1aeb1398f7ed55b00c5a1
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-01-30 13:04:08 +0000

    contact links added

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -12,9 +12,16 @@ export default function Contacts() {
         <section className="page" id="contacts">
             <h1 className="section-title">Contacts</h1>
             <div className="section-sub">
-                <h5><img style={{ height: '2rem' }} src={emailLogo} alt={emailLogo} /> Email</h5>
-                <h5><img style={{ height: '2rem' }} src={gitHubIcon} alt={gitHubIcon} /> GitHub</h5>
-                <h5><img style={{ height: '2rem' }} src={linkedInLogo} alt={linkedInLogo} /> LinkedIn</h5>
+                <a href='mailto:annelaure.guiot@gmail.com' target="_blank">
+                    <h5><img style={{ height: '2rem' }} src={emailLogo} alt={emailLogo} /> Email</h5>
+                </a>
+                <a href='https://www.linkedin.com/in/anne-laure-guiot/' target="_blank">
+                    <h5><img style={{ height: '2rem' }} src={linkedInLogo} alt={linkedInLogo} /> LinkedIn</h5>
+                </a>
+                <a href='https://github.com/Annel18' target="_blank">
+                    <h5><img style={{ height: '2rem' }} src={gitHubIcon} alt={gitHubIcon} /> GitHub</h5>
+                </a>
+
             </div >
         </section>
     )
