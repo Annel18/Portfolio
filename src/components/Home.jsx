e1abe882eef541a2fb55a4bbbb4267a94e825ddc
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-01-31 12:53:16 +0000

    className instead of class property corrected in jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,13 @@ export default function Home() {
     return (
         <>
             <section id="home">
-                <div class="animated-title">
-                    <div class="text-top">
+                <div className="animated-title">
+                    <div className="text-top">
                         <div>
                         <span>Anne-Laure Guiot</span>
                         </div>
                     </div>
-                    <div class="text-bottom">
+                    <div className="text-bottom">
                         <div>Junior Software Engineer</div>
                     </div>
                 </div>
