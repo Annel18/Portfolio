c271879e60a48b62966c9a1b61409ab618033625
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-05-09 11:52:43 +0100

    data placed in specific folder

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,31 +1,13 @@
 /* eslint-disable react/prop-types */
 // import { Link } from 'react-router-dom'
 import { Container, Row, Col } from "react-bootstrap"
-import knitLogo from '../assets/icons/knittingLogo.svg'
-import circuit from '../assets/icons/circuit.svg'
-import musicLogo from '../assets/icons/musicLogo.svg'
+import Interests from '../assets/data/Interests.js'
 
 
 
 export default function Interests() {
 
-    const Interests = [
-        {
-            title: 'Knitting',
-            img: knitLogo,
-            description: '<p>Since learning knitting two years ago, I have been making many clothes (hats, scarves, cardigans, hoodies..) and have become obsessed with learning new techniques and creating new patterns.</p>'
-        },
-        {
-            title: 'Electronics',
-            img: circuit,
-            description: '<p>I enjoy fiddling with Processing and Arduino and I recently made a touch lamp with conductive paint, LED strips and frosted glass. Playing with Arduino breadboards kept me busy in lockdown.</p>'
-        },
-        {
-            title: 'Music',
-            img: musicLogo,
-            description: '<p style="padding-bottom:3rem;">I also enjoy playing the violin and piano as a hobby.</p>'
-        }
-    ]
+
 
     return (
         <section className="page" id="interests">
